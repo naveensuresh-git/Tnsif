dc4f4512ff80a7c6fa834fb96c0346857bbaaf1d
navee <navee@NAVEEN>
2025-10-24 20:49:26 +0530

    commit

--- javaprograms/src/com/tnsif/daysix/usingfinal/FinalClassDemo.java
+++ javaprograms/src/com/tnsif/daysix/usingfinal/FinalClassDemo.java
@@ -1,4 +1,3 @@
-//Program to demonstrate final class
 package com.tnsif.daysix.usingfinal;
 
 final class FinalClass {
@@ -7,14 +6,9 @@ final class FinalClass {
 	}
 }
 
-//can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
-class FinalChildClass extends FinalClass {
-}
-
 public class FinalClassDemo {
 	public static void main(String[] args) {
-		// Create the object of final class
-		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
+		FinalClass f1 = new FinalClass();
 		f1.show();
 	}
 }
